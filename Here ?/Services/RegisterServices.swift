395f9bd7db0a5a0c05cae6a4cecb631a6e65ecd8
AsmaaTarek <asmaatarek93@gmail.com>
2019-06-15 11:14:12 +0200

    changes

--- Here ?/Services/RegisterServices.swift
+++ Here ?/Services/RegisterServices.swift
@@ -15,7 +15,7 @@ class RegisterServices{
    
         Alamofire.request(authURL, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header).responseString { (response) in
             if  response.result.error == nil{
-                LocalStore.sharedLocalStore.isLoggedIn = true
+                //LocalStore.sharedLocalStore.isLoggedIn = true
 
                 completion (true)
             }else{
